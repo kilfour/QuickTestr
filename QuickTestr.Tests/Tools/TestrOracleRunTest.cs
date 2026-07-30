d08d28781415450b115ff9734563f9f55c8af843
kilfour <kilfour@gmail.com>
2026-07-30 08:00:44 +0200

    test cleanup

--- QuickTestr.Tests/Tools/TestrOracleRunTest.cs
+++
@@ -1,9 +0,0 @@
-using QuickCheckr.Protocol;
-using QuickTestr.Bolts.ClerksOffice;
-
-namespace QuickTestr.Tests.Tools;
-
-public abstract class TestrOracleRunTest<T> : TestrRunTest<T>
-{
-    protected override ITranscribe Clerk => new OracleClerk();
-}
--- QuickTestr.Tests/Tools/TestrPropertyRunTest.cs
+++
@@ -1,9 +0,0 @@
-using QuickCheckr.Protocol;
-using QuickTestr.Bolts.ClerksOffice;
-
-namespace QuickTestr.Tests.Tools;
-
-public abstract class TestrPropertyRunTest<T> : TestrRunTest<T>
-{
-    protected override ITranscribe Clerk => new PropertyClerk();
-}
